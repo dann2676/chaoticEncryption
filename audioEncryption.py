9ef6e9edb4aa89dbbe23eb3a9367a50b749858e4
Daniela Espinosa <danielaespinosa@keos.co>
2018-09-27 16:22:07 -0500

    probar el tiempo

--- audioEncryption.py
+++ audioEncryption.py
@@ -1,31 +1,28 @@
-from ClassH import Protocol,Henon
+from henonClasses import Protocol, Henon
 import numpy as np
 import threading
-import time
-#Parametros Iniciales
+import pyaudio
+from time import time
 
+#Parametros Iniciales
 x1 = 0
 y1 = 0
 x2 = 1
 y2 = 3
 
-#Creación de de atractores
-m = Henon (x1, y1)
-e = Henon (x2, y2)
+#Creacion de de atractores
+master = Henon (x1, y1)
+slave = Henon (x2, y2)
 
 #Relacionar los actractores
-sender= Protocol(m)
-receiver =Protocol(e)
+sender= Protocol(master)
+receiver =Protocol(slave)
 
 #sincronizar
-
-sender.syncrhonize (e)
-
+sender.syncrhonize (slave)
 
 
-import pyaudio
-
-
+#Definicion formato de audio wav
 CHUNK = 1024 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
 
@@ -33,11 +30,12 @@ p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               output=True,frames_per_buffer=CHUNK) #uses default input device
 
-
+#funcion que recibe array con los datos de audio y stream para reproduccion del audio.
 def proceso(data, stream):
+
     #stream.write(data2,CHUNK)
     #print (data)
-    
+
     #start_time = time.time()
     encrypt_sound = sender.encrypt(data.copy())
     #stream.write(encrypt_sound, CHUNK)
@@ -53,11 +51,13 @@ def proceso(data, stream):
 
 
 for i in range(500): #to it a few times just to see
+    start_time=time()
     data2 =stream.read(CHUNK)
     data = np.fromstring(data2, dtype=np.int16)
-    proceso(data, stream)
-    
-    
+    print(time()-start_time)
+    #proceso(data, stream)
+
+
 stream.stop_stream()
 stream.close()
 p.terminate()
